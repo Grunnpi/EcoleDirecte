30bc857e469005f8f22d98148667e79e5c40c808
Grunnpi <pierre.grunnagel@gmail.com>
2020-03-26 16:51:23 +0100

    telegram + google doc version

--- EcoleDirecte.py
+++ EcoleDirecte.py
@@ -4,10 +4,38 @@ import os
 import argparse
 import csv
 import os.path
+import urllib.parse
+
+from requests.packages.urllib3.exceptions import InsecureRequestWarning
+
+import telegram
+
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
+
+requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
+
+
+EcoleDirectVersion = 'v1.0'
 
 proxies = {}
-
 sep = ","
+
+
+class UnEnfant:
+    "Notes"
+    prenom = ''
+    onglet = ''
+
+    def __init__(self):
+        self.prenom = ''
+    def __eq__(self, other):
+        """Comparaison de deux au pair"""
+        return self.prenom == other.prenom
+    def toString(self, sep):
+        """Format du dump fichier"""
+        return self.prenom \
+               + sep + self.onglet
 
 class UneNote:
     "Notes"
@@ -39,7 +67,10 @@ class UneNote:
         if self.periode == other.periode:
             if self.libelleMatiere == other.libelleMatiere:
                 if self.date == other.date:
-                    return self.valeur < other.valeur
+                    if ( str(self.valeur).isnumeric() and str(other.valeur).isnumeric() ):
+                        return self.valeur < other.valeur
+                    else:
+                        return -1
                 else:
                     return self.date < other.date
             else:
@@ -69,97 +100,63 @@ def dump( champ, bulletProof ):
     returnMe = returnMe.replace("'", "\"")
     return returnMe
 
+def listeNoteGoogle(sheetOnglet):
+    all_kid_notes = []
+    all_kid_notes_sheet = sheetOnglet.get_all_records()
+    #
+    for rec in all_kid_notes_sheet:
+        uneNote = UneNote('', '', '', '', '', '', '', '')
+        for item in rec.items():
+            # print(item[0], " -- ", item[1], "<", item, ">",)
+            if ( item[0] == 'periode'):
+                uneNote.periode = item[1]
+            if ( item[0] == 'libelleMatiere'):
+                uneNote.libelleMatiere = item[1]
+            if ( item[0] == 'valeur'):
+                uneNote.valeur = item[1]
+            if ( item[0] == 'noteSur'):
+                uneNote.noteSur = item[1]
+            if ( item[0] == 'coef'):
+                uneNote.coef = item[1]
+            if ( item[0] == 'typeDevoir'):
+                uneNote.typeDevoir = item[1]
+            if ( item[0] == 'devoir'):
+                uneNote.devoir = item[1]
+            if ( item[0] == 'date'):
+                uneNote.date = item[1]
+        all_kid_notes.append(uneNote)
+
+    all_kid_notes = sorted(all_kid_notes)
+    return all_kid_notes
+
 # fonction pour lister toutes les notes d'un eleve sur base de son ID
-def listeNote(eleve_id, eleve_prenom, token):
+def listeNoteSite(eleve_id, token):
+    all_kid_notes = []
+
     payloadNotes = "data={\"token\": \"" + token + "\"}"
     headersNotes = {'content-type': 'application/x-www-form-urlencoded'}
-
     r = requests.post("https://vmws17.ecoledirecte.com/v3/eleves/" + str(eleve_id) + "/notes.awp?verbe=get&",
                       data=payloadNotes, headers=headersNotes, proxies=proxies, verify=False)
     if r.status_code != 200:
         print(r.status_code, r.reason)
-    # print(r.content)
-
-    compteurNote = 0
-    compteurNouvelleNote = 0
-
     notesEnJSON = json.loads(r.content)
     if len(notesEnJSON['data']['notes']) > 0:
-        nomDuFichier = eleve_prenom + '.csv'
-
-        notesSite = []
-        notesFichier = []
         for note in notesEnJSON['data']['notes']:
             uneNote = UneNote( \
-                    dump(note['codePeriode'], False) \
-                ,   dump(note['libelleMatiere'], True) \
-                ,   dump(note['valeur'].replace(",", "."), False) \
-                ,   dump(note['noteSur'], True) \
-                ,   dump(note['coef'], True) \
-                ,   dump(note['typeDevoir'], False) \
-                ,   dump(note['devoir'], False) \
-                ,   dump(note['date'], True) \
+                    note['codePeriode'] \
+                ,   note['libelleMatiere'] \
+                ,   note['valeur'].replace(".", ",") \
+                ,   note['noteSur'] \
+                ,   note['coef'] \
+                ,   note['typeDevoir'] \
+                ,   note['devoir'] \
+                ,   note['date'] \
                 )
-            notesSite.append(uneNote)
-
-        nouveauFichier = True
-        if ( os.path.isfile(nomDuFichier) ):
-            nouveauFichier = False
-            # lire les notes déjà presente si existe
-            line_count = 0
-            with open(nomDuFichier) as csv_file:
-                csv_reader = csv.reader(csv_file, delimiter=sep)
-                for row in csv_reader:
-                    if line_count == 0:
-                        #print(f'Column names are {", ".join(row)}')
-                        line_count += 1
-                    else:
-                        #print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                        uneNote = UneNote( \
-                            dump(row[0], False) \
-                            ,   dump(row[1], True) \
-                            ,   dump(row[2], False) \
-                            ,   dump(row[3], True) \
-                            ,   dump(row[4], True) \
-                            ,   dump(row[5], False) \
-                            ,   dump(row[6], False) \
-                            ,   dump(row[7], True) \
-                            )
-                        notesFichier.append(uneNote)
-                        line_count += 1
-
-        notesSite = sorted(notesSite)
-        notesFichier = sorted(notesFichier)
-
-        with open(nomDuFichier, "a") as n:
-            if ( nouveauFichier ):
-            # header du fichier
-                n.write(
-                    'periode'
-                    + sep + 'libelleMatiere'
-                    + sep + 'valeur'
-                    + sep + 'noteSur'
-                    + sep + 'coef'
-                    + sep + 'typeDevoir'
-                    + sep + 'devoir'
-                    + sep + 'date'
-                    + "\n"
-                )
-            for note in notesSite:
-                compteurNote = compteurNote + 1
-                nouvelleNote = True
-                for ancienneNote in notesFichier:
-                    if ( note == ancienneNote ):
-                        nouvelleNote = False
-                        break
-                if ( nouvelleNote ):
-                    n.write( note.toString(sep) + "\n" )
-                    compteurNouvelleNote = compteurNouvelleNote + 1
-
-            print('[' + str(compteurNouvelleNote) + '] nouvelles notes, pour un total de [' + str(compteurNote) + '] notes')
+            all_kid_notes.append(uneNote)
+        all_kid_notes = sorted(all_kid_notes)
     else:
         print("pas de notes encore")
-    return compteurNouvelleNote
+    return all_kid_notes
 
 
 # partie principale
@@ -167,15 +164,40 @@ if __name__ == "__main__":
 
     parser=argparse.ArgumentParser()
 
+    # Ecole Directe cred
     parser.add_argument('--user', help='User', type=str)
     parser.add_argument('--pwd', help='Password', type=str)
     parser.add_argument('--proxy', help='https://uzer:pwd@name:port', type=str, default="")
+    # credential google
+    parser.add_argument('--cred', help='true', type=str, default="")
+    # telegram mode
+    parser.add_argument('--token', help='true', type=str, default="")
+    parser.add_argument('--chatid', help='true', type=str, default="")
+    parser.add_argument('--telegram', help='true', type=str, default="no")
+
     args=parser.parse_args()
 
-    #print(args.user)
-    #print(args.pwd)
-    #print(args.proxy)
-    print("sboub")
+    # use creds to create a client to interact with the Google Drive API
+    scope = ['https://spreadsheets.google.com/feeds','https://www.googleapis.com/auth/drive']
+    creds = ServiceAccountCredentials.from_json_keyfile_name(str(args.cred), scope)
+    client = gspread.authorize(creds)
+
+    # collect all kids and related setup
+    notes_ConfigurationSheet = client.open("Notes_EcoleDirecte").worksheet("Configuration")
+    list_configuration = notes_ConfigurationSheet.get_all_records()
+    listeEnfants = []
+    for rec in list_configuration:
+        unEnfant = UnEnfant()
+        for item in rec.items():
+            if ( item[0] == 'Prénom'):
+                unEnfant.prenom = item[1]
+                print(item[1])
+            if ( item[0] == 'Onglet'):
+                unEnfant.onglet = item[1]
+                print(item[1])
+        listeEnfants.append(unEnfant)
+
+    print("**** sboub")
 
     if args.proxy:
         print("Proxy provided")
@@ -185,6 +207,7 @@ if __name__ == "__main__":
 
     payload = "data={\"identifiant\": \"" + str(args.user) + "\", \"motdepasse\" : \"" + str(args.pwd) + "\"}"
     headers = {'content-type': 'application/x-www-form-urlencoded'}
+    payload = urllib.parse.quote_plus(payload)
     #print(payload)
 
     r = requests.post("https://vmws06.ecoledirecte.com/v3/login.awp", data=payload, headers=headers, proxies=proxies, verify=False)
@@ -193,11 +216,61 @@ if __name__ == "__main__":
     retourEnJson = json.loads(r.content)
     print("Generation des fichiers ici : [" + os.getcwd() + "]")
 
+    telegram_message = "*EcoleDirect(" + EcoleDirectVersion + ")* "
     compteurTotalNouvelleNote = 0
     for eleve in retourEnJson['data']['accounts'][0]['profile']['eleves']:
         eleveId = str(eleve['id'])
         elevePrenom = str(eleve['prenom'])
         print("Eleve(" + eleveId + ")[" + elevePrenom + "]")
-        compteurTotalNouvelleNote = compteurTotalNouvelleNote + listeNote(eleveId, elevePrenom, retourEnJson['token'])
+        trouveEleve = False
+        nbCreate = 0
+        for x in listeEnfants:
+            if x.prenom == elevePrenom:
+                print(">> Eleve config trouvé : ", x.onglet)
+                trouveEleve = True
+                print(">>> Extract Google")
 
+                sheet_ongleNotes = client.open("Notes_EcoleDirecte").worksheet(x.onglet)
+                eleveNotesDansGoogle = listeNoteGoogle(sheet_ongleNotes)
+                print(">>> Extract Site")
+                eleveNotesDansSite = listeNoteSite(eleveId, retourEnJson['token'])
+
+                print("Notes dans google[", len(eleveNotesDansGoogle), "] / notes sur site [", len(eleveNotesDansSite), "]")
+                googleNextRow = len(eleveNotesDansGoogle) + 2 # header + new row
+
+                for uneNoteSite in eleveNotesDansSite:
+                    isNoteSiteDejaSurGoogle = False
+                    for uneNoteGoogle in eleveNotesDansGoogle:
+                        if ( uneNoteSite == uneNoteGoogle ):
+                            isNoteSiteDejaSurGoogle = True
+                            break
+                    if ( not isNoteSiteDejaSurGoogle ):
+                        print("Ajoute %s" % uneNoteSite.valeur, " @ ligne %d" % googleNextRow)
+                        theValeur = uneNoteSite.valeur
+                        if ( uneNoteSite.valeur.isnumeric()):
+                            theValeur = float(theValeur)
+                        theNoteSur = uneNoteSite.noteSur
+                        if ( uneNoteSite.noteSur.isnumeric()):
+                            theNoteSur = float(theNoteSur)
+                        theCoef = uneNoteSite.coef
+                        if ( uneNoteSite.coef.isnumeric()):
+                            theCoef = float(theCoef)
+
+                        row = [uneNoteSite.periode, uneNoteSite.libelleMatiere, theValeur, theNoteSur, theCoef, uneNoteSite.typeDevoir, uneNoteSite.devoir, uneNoteSite.date]
+                        print(sheet_ongleNotes.insert_row(row, googleNextRow))
+                        googleNextRow = googleNextRow + 1
+                        nbCreate = nbCreate + 1
+                    else:
+                        print("Note %s" % uneNoteSite.valeur, " @ déjà présente")
+                print("Nombre de notes ajoutées pour ",elevePrenom," = ", nbCreate)
+                telegram_message = telegram_message + " / "  + elevePrenom + ":`" + str(nbCreate) + "`"
+
+                break
+        if ( not trouveEleve ):
+            print(">> Eleve config non trouvé !!")
     print("Fin extraction.\nTotal nouvelles notes=" + str(compteurTotalNouvelleNote))
+
+    if ( str(args.telegram) == "yes" ) :
+        bot = telegram.Bot(token=str(args.token))
+        bot.send_message(chat_id=str(args.chatid), text=telegram_message, parse_mode=telegram.ParseMode.MARKDOWN)
+
